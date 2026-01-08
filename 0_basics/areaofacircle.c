adb81dbb226e013e45b27a40695555ccc39ad0ff
Atul Singh <satul1628@gmail.com>
2026-01-08 22:32:22 +0530

    Delete 0_basics directory

--- 0_basics/areaofacircle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float pi,radius,area;
-    pi = 3.1415;
-    radius = 10;
-    area = pi*radius*radius;
-    printf("THE AREA OF CIRCLE IS:%f",area);
-    return 0;
-}
--- 0_basics/volumeofasphere.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float radius,volume,pi;
-    radius = 10;
-    pi = 3.1415;
-    volume = (4*pi*radius*radius*radius)/3;
-    printf("THE VOLUME OF SPHERE IS:%f",volume); 
-    return 0;
-}
