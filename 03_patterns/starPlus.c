143e351eb0372acc725877edeb8a7e694c3b04d1
Atul Singh <satul1628@gmail.com>
2026-01-11 22:17:19 +0530

    Modify starPlus.c to print spaces instead of '#'
    Updated the star pattern program to print spaces instead of '#' for better visual representation.

--- 03_patterns/starPlus.c
+++ 03_patterns/starPlus.c
@@ -4,12 +4,13 @@ int main(){
     printf("ENTER A NUMBER FOR PATTERN PRINTING:");
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        int a=n/2+1;
+        int a=n/2+1;// the condition by which we get the middle row and column of pattern to apply condition 
         for(int j=1;j<=n;j++){
             if(i==a || j==a) printf("*");
-            else printf("#");// for printing only a star plus without # u need to print space
+            else printf(" ");// for printing star plus we need to print space here instesd of hash
         }
         printf("\n");
     }
     return 0;
+
 }
