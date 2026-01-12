0f7676878874d2e416b90fc77952c70d3c7722a8
Atul Singh <satul1628@gmail.com>
2026-01-12 11:09:02 +0530

    Refactor to print Floyd's Triangle
    Updated the implementation to print Floyd's Triangle with incrementing values.

--- 03_patterns/floydsTriangle.c
+++ 03_patterns/floydsTriangle.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main(){
+    // 1      FLOYDS TRIANGLE
+    // 2 3
+    // 4 5 6 
+    // 7 8 9 10
     int n;
     printf("ENTER A NUMBER FOR PATTERN PRINTING:");
     scanf("%d",&n);
-    for(int i=1;i<=n;i++){
+    int a=1;
+    for(int i=1;i<=n;i++){// these loops are only creating the stucture of the triangle printing the values of a 
         for(int j=1;j<=i;j++){
-            printf("%d",j);
+            printf("%d ",a);
+            a++;// incrementing a after every iteration of loop
         }
         printf("\n");
     }
     return 0;
+
 }
