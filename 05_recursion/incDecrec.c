fac26b3abbaaca6165d9c0830d624d44851b0718
Atul Singh <satul1628@gmail.com>
2026-01-19 22:09:21 +0530

    Enhance incDecrec.c with comments for clarity
    Added comments to explain the functionality of the incdec function.

--- 05_recursion/incDecrec.c
+++ 05_recursion/incDecrec.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 void incdec(int n){
-    if(n==0) return;
-    printf("%d\n",n);
+    if(n==0) return;// base case
+    printf("%d\n",n); // will print decreasing number till base case
     incdec(n-1);
-    printf("%d\n",n);
+    printf("%d\n",n);// this will print increasing number using back tracking
 }
 int main(){
     int n;
     printf("ENTER A NUMBER: ");
     scanf("%d",&n);
-    incdec(n);
+    incdec(n);// function call
     return 0;
+
 }
