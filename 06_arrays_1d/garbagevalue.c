51de47f62b4d62f332ddbe2bbaae88a12183856e
Atul Singh <satul1628@gmail.com>
2026-01-25 20:30:20 +0530

    Delete 06_arrays_1d directory

--- 06_arrays_1d/garbagevalue.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int arr[4];
-    printf("%d\n",arr[0]);
-    printf("%d\n",arr[1]);
-    printf("%d\n",arr[2]);
-    printf("%d\n",arr[3]);
-    return 0;
-}
--- 06_arrays_1d/prinarrayloop.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int arr[5]={1,2,3,4,5};
-    for(int i=0;i<=4;i++)// WHERE I IS 0,1,2,3,4 WHICH ARE THE INDEX VALUES 
-    printf("%d\n",arr[i]);
-    return 0;
-}
